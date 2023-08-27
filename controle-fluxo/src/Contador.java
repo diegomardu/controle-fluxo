605828ed644d7595a136ad2899c1415ff7c77ea6
diegomardu <diegomartins836@gmail.com>
2023-08-27 18:28:25 -0300

    removendo comentarios.

--- controle-fluxo/src/Contador.java
+++ controle-fluxo/src/Contador.java
@@ -10,7 +10,7 @@ public class Contador {
 		int parametroDois = terminal.nextInt();
 		
 		try {
-			//chamando o método contendo a lógica de contagem
+			
 			contar(parametroUm, parametroDois);
 		
 		}catch (ParametrosInvalidosException exception) {
@@ -24,7 +24,7 @@ public class Contador {
 			throw new ParametrosInvalidosException();
 		}
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+		
 		for(int i = 0; i < contagem; i++) {
 			System.out.println("Imprimindo o número " + i);
 		}
